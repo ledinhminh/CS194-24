df177dc22bfeba84ae54858619efe94d5ebf852f
Timmy Zhu <timothy.zhu@berkeley.edu>
2013-02-24 21:01:09 -0500

    Add the CGI interface variables Add non-executable /webclock.dl

--- httpd/mimetype_cgi.c
+++ httpd/mimetype_cgi.c
@@ -13,6 +13,16 @@
 
 #define BUF_COUNT 4096
 
+// Defined in main.c
+#define PORT 8088
+
+// Some CGI environment variables, I guess.
+#define CGI_SERVER_SOFTWARE "cs194-24/1.0"
+#define CGI_SERVER_NAME "127.0.0.1"
+#define CGI_GATEWAY_INTERFACE "CGI/1.1"
+
+#define CGI_SERVER_PROTOCOL "HTTP/1.1"
+
 static int http_get(struct mimetype *mt, struct http_session *s);
 
 struct mimetype *mimetype_cgi_new(palloc_env env, const char *fullpath)
@@ -42,6 +52,8 @@ int http_get(struct mimetype *mt, struct http_session *s)
     char* query_string;
     char* real_path;
     int real_path_len; 
+    char* env_vars;
+    int env_vars_len;
 
     mtc = palloc_cast(mt, struct mimetype_cgi);
     if (mtc == NULL)
@@ -57,11 +69,43 @@ int http_get(struct mimetype *mt, struct http_session *s)
     *(real_path + real_path_len) = '\0';
     INFO; printf("fullpath=%s, real_path_len=%d, real_path=%s\n", mtc->fullpath, real_path_len, real_path);
     
-    if (NULL != query_string)
+    if (NULL == query_string) {
+        // Well, we have to have something to give to the program.
+        query_string = "";
+    } else {
         INFO; printf("query string: %s\n", query_string);
+    }
+    
+    // Prepare the environment variables.
+    // There has to be a better way of doing this.
+    env_vars_len = snprintf(NULL, 0, \
+        "%s='%s' %s='%s' %s='%s' %s='%s' %s='%d' %s='%s' %s='%s' ", \
+        "SERVER_SOFTWARE", CGI_SERVER_SOFTWARE, \
+        "SERVER_NAME", CGI_SERVER_NAME, \
+        "GATEWAY_INTERFACE", CGI_GATEWAY_INTERFACE, \
+        "SERVER_PROTOCOL", CGI_SERVER_PROTOCOL, \
+        "SERVER_PORT", PORT, \
+        // main.c:75
+        "REQUEST_METHOD", "GET", \
+        "QUERY_STRING", query_string
+        ) + 1;
+    env_vars = palloc_array(s, char, env_vars_len + strlen(real_path));
+    snprintf(env_vars, env_vars_len, \
+        "%s='%s' %s='%s' %s='%s' %s='%s' %s='%d' %s='%s' %s='%s' ", \
+        "SERVER_SOFTWARE", CGI_SERVER_SOFTWARE, \
+        "SERVER_NAME", CGI_SERVER_NAME, \
+        "GATEWAY_INTERFACE", CGI_GATEWAY_INTERFACE, \
+        "SERVER_PROTOCOL", CGI_SERVER_PROTOCOL, \
+        "SERVER_PORT", PORT, \
+        "REQUEST_METHOD", "GET", \
+        "QUERY_STRING", query_string
+        );
+        
+    strcpy(env_vars + env_vars_len - 1, real_path);
+    INFO; printf("env_vars=%s\n", env_vars);
 
     // No, it cannot be O_RDONLY.
-    fp = popen(real_path, "r"); 
+    fp = popen(env_vars, "r"); 
     if (NULL == fp) {
         INFO; printf("popen returned NULL; strerror: %s\n", strerror(errno));
         return -1;
@@ -84,6 +128,8 @@ int http_get(struct mimetype *mt, struct http_session *s)
 	}
     }
     pclose(fp);
+    
+    pfree(real_path);
 
     return 0;
 }
